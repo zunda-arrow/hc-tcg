b5fc7038d5396428ded8fe327e5a3c9607707103
Lunarmagpie <Bambolambo0@gmail.com>
2025-02-05 21:29:07 -0500

    Add vritual file

---
+++ common/virtual/virtual-action.ts
@@ -0,0 +1,18 @@
+import {AIComponent} from 'common/components/ai-component'
+import {delay, put} from 'typed-redux-saga'
+import {ServerGameController} from '../../game-controller'
+import {LocalMessage, localMessages} from '../../messages'
+
+export default function* virtualPlayerActionSaga(
+	con: ServerGameController,
+	component: AIComponent,
+) {
+	const coinFlips = con.game.currentPlayer.coinFlips
+	yield* delay(con.getRandomDelayForAI(coinFlips))
+	const action = component.getNextTurnAction()
+	yield* put<LocalMessage>({
+		type: localMessages.GAME_TURN_ACTION,
+		action: action,
+		playerEntity: component.playerEntity,
+	})
+}
